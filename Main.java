76bd00b4af058630d6a108fdb4cc27feabe60772
Sarayu-chityala <sarayuchityala15@gmail.com>
2025-06-17 15:29:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String args[]){
+int a=2;
+int b=3;
+System.out.println("Addition"+(a+b));
+System.out.println("Substartion"+(a-b));
+System.out.println("MUltiplication"+(a*b));
+System.out.println("Division"+(a/b));
+System.out.println("Modulo"+(a%b));
+}
+}
